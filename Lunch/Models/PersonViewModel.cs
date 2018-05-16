9e65e7cc971d6898a918b266162ad51350bf736c
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-05-16 19:41:52 -0400

    Completed Person models and controllers;

--- Lunch/Models/PersonViewModel.cs
+++ Lunch/Models/PersonViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Lunch.Models
+﻿using System.ComponentModel;
+
+namespace Lunch.Models
 {
     public class PersonViewModel
     {
         public int? PersonId { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
         public string FullName => FirstName + " " + LastName;
     }
